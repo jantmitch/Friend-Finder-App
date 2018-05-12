58bdc276dc98777ed3338819bfd9c3d3ac8d751a
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-05-12 10:32:22 -0400

    update routes

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-require("./routing/apiRoutes.js");
-require("./routing/htmlRoutes.js");
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
+
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT)
