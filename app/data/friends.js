37892b6569911a76dc681bc30d36edcdf2a96da4
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-05-12 10:28:36 -0400

    update files

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+{
+    name:"Scully",
+    photo: "https://carboncostume.com/wordpress/wp-content/uploads/2015/09/danascully.jpg",
+   scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
