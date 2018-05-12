beb7b2ffda6585c295c5de3d70065c98e384f490
Anthony Mitchell <j.ant.mitch@gmail.com>
2018-05-12 15:51:51 -0400

    update JS on survey.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,5 @@
-{
+[
+    {
     name:"Scully",
     photo: "https://carboncostume.com/wordpress/wp-content/uploads/2015/09/danascully.jpg",
    scores: [
@@ -13,4 +14,21 @@
         4,
         1
       ]
-  }
+    },
+      {
+          name: "Mulder",
+          photo: "https://cdn-images-1.medium.com/max/1600/1*u8bnn6Hb1Y6JaVtVO0gpbw.jpeg",
+          scores: [
+              5,
+              3,
+              4,
+              5,
+              1,
+              2,
+              3,
+              4,
+              5,
+              5
+          ]
+      }
+  ]
